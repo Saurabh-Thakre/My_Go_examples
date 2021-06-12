0da2c8187f68898527c4bf485b1bd9f369aae206
saurabh-thakre <thakre.sau@gmail.com>
2021-06-12 13:36:34 +0530

    2nd

--- practice/main.go
+++ practice/main.go
@@ -1,37 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 const aConst int = 65
 
 func main() {
 
-	var aString string = "This is the String in Go"
-
-	fmt.Println(aString)
-	fmt.Printf("This variable's type is %T\n", aString)
-
-	var aInteger int = 100
-	fmt.Println(aInteger)
-
-	var defaultInt int
-	fmt.Println(defaultInt)
-
-	var anotherString = "This is another String"
-	fmt.Println(anotherString)
-	fmt.Printf("This variable's type is %T\n", anotherString)
-
-	var anotherInt = 45
-	fmt.Println(anotherInt)
-	fmt.Printf("This variable's type is %T\n", anotherInt)
-
-	myString := "This is also a String but without var"
-	fmt.Println(myString)
-	fmt.Printf("This variable's type is %T\n", myString)
-
-	fmt.Println(aConst)
-	fmt.Printf("This variable's type is %T\n", aConst)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Entered String :", input)
 
 }
