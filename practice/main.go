26e9d4988c6d12b91f560b34ef90a3044f94736d
saurabh-thakre <thakre.sau@gmail.com>
2021-06-14 12:21:56 +0530

    test

--- practice/main.go
+++ practice/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-const aConst int = 65
-
 func main() {
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("Entered String :", input)
 
 }
