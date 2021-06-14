4878889963b01094169634e3893c88bdc30c9340
saurabh-thakre <thakre.sau@gmail.com>
2021-06-14 18:32:55 +0530

    switch statement

--- practice/main.go
+++ practice/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"math/rand"
+	"time"
 )
-
-const aConst int = 65
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("Entered String :", input)
+	rand.Seed(time.Now().Unix())
+	dow := rand.Intn(7) + 1
+	fmt.Println("Day", dow)
+
+	var result string
+
+	switch dow {
+	case 1:
+		result = "Wow ! It's Sunday o/"
+	case 2:
+		result = "It's Monday, Go to Sleep"
+	default:
+		result = "It's some other day !!"
+	}
+
+	fmt.Println(result)
 
 }
