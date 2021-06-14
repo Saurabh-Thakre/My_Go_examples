feee0e458e7aa51039ef78ef1e3336571d5d4345
saurabh-thakre <thakre.sau@gmail.com>
2021-06-14 12:57:43 +0530

    3rd, struct

--- practice/main.go
+++ practice/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+//Structure
+
+func main() {
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+
+}
+
+// Here Dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
 }
